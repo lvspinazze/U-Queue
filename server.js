df140865c50e5d07ec4130393474c1c82a89694a
Lauren Spinazze <lvspinazze@Laurens-MacBook-Pro.local>
2018-05-27 20:09:20 -0400

    yarn version

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const passport = require('passport');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
